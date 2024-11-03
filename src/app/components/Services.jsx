a742693ed06c457579b5db19a7cdc60d073a30c3
hbghaffar01 <hbghaffar01@gmail.com>
2024-11-04 01:14:51 +0400

    feat:updated

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -8,7 +8,7 @@ import SectionWrapper from "../hoc/SectionWrapper";
 const Services = () => {
   return (
     <div className={`${styles.paddingX} pt-0 md:pb-24 xs:pb-6 w-full`}>
-      <div className="flex items-center justify-between flex-wrap">
+      <div className="flex items-center md:justify-between xs:justify-center flex-wrap">
         {servicesItems.map((service) => (
           <ServiceCard
             key={service.id}
